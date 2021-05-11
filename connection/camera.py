c5164c4f426e1dbacb7e14b063555e94f558811f
ThomasLuchies <Thomasluchies22@gmail.com>
2021-05-11 10:18:19 +0200

    camera upate

--- connection/camera.py
+++ connection/camera.py
@@ -1,18 +1,15 @@
 import cv2
+import socket
+
 def MainVideo():
-    print('Press x to exit')
-    capt = cv2.VideoCapture('udp://0.0.0.0:11111')
+    capt = cv2.VideoCapture("udp://0.0.0.0:11111")
 
     while True:
+        
         result, frame = capt.read() 
         cv2.imshow('Tello video', frame)
 
         key = cv2.waitKey(1) & 0xff 
-        if key == ord('x'): 
-            break
-        capt.release() 
-        cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     MainVideo()
-    
